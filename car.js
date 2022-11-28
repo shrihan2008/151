f00fbb153c80ae7d51b19b81fe498f0d0bc571eb
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-11-28 16:54:43 +0530

    Add files via upload

--- car.js
+++ car.js
@@ -2,9 +2,12 @@ AFRAME.registerComponent('car',{
     schema:{
         modelRef: { type: "string", default: "../car/scene.gltf" }
     },
-    init:function(){
-        console.log(this.data.modelRef)
+    init: function() { 
+        this.el.setAttribute("gltf-model", this.data.modelRef); 
+        const position = { x: 0, y: 50, z: 80 };
+         const rotation = { x: 0, y: -100, z: 0 }; 
+         this.el.setAttribute("position", position); 
+         this.el.setAttribute("rotation", rotation);
     }
-
 
 })
